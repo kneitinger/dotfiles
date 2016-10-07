fdc37b70fe310a349b7434251b839d41b2d6d2f7
Kyle Kneitinger <kylejkneitinger@gmail.com>
2016-10-06 20:09:07 -0700

    Modified i3 config generating scripts to look work on ~/.i3 directory

--- i3/i3-file-watch.sh
+++ i3/i3-file-watch.sh
@@ -2,7 +2,7 @@
 
 MODEL=$(tr "[:blank:]" "_" < /sys/devices/virtual/dmi/id/product_version | tr "[:upper:]" "[:lower:]")
 
-cd "$HOME/i3" || exit
+cd "$HOME/.i3" || exit
 
 inotifywait -m -r -e modify  --format '%w%f' ./core ./"$MODEL" | while read -r FILE
 do
