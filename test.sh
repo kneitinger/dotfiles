43bddc9e18b3a9b4614799f9619eccf551cd66b4
Kyle Kneitinger <kyle@kneit.in>
2016-12-23 00:37:40 -0800

    Add shellcheck based Travis CI tests

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Taken from Jess Frazelle at:
+# https://github.com/jessfraz/dotfiles/blob/master/test.sh
+
+set -e
+
+# find all executables and run `shellcheck`
+FILES=$(find . -type f -not -path "*/\.*" -not -name "LICENSE" -not -name "README.md")
+
+for file in $FILES; do
+  shellcheck "$file"
+  echo "[ok] $file shellcheck pass"
+done
