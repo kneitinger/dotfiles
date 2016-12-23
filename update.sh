264bfe83dd4e788370d1e0f75ca8b61be651e58b
Kyle Kneitinger <kyle@kneit.in>
2016-12-22 21:47:24 -0800

    Add Jessie Frazelle's update.sh script

---
+++ update.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Credit to Jessie Frazelle: https://github.com/jessfraz/.vim/blob/master/update.sh
+
+set -e
+
+git submodule update --init --recursive
+git submodule foreach git pull --recurse-submodules origin master
