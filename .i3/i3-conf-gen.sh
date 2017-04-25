b2bd594d8fde3128b9cb63342b6bbebb18ac7238
Kyle Kneitinger <kyle@kneit.in>
2017-04-24 20:47:15 -0700

    Swapped printf for echo -e in i3-conf-gen for better portability

--- .i3/i3-conf-gen.sh
+++ .i3/i3-conf-gen.sh
@@ -9,12 +9,11 @@ if [ -e "$HOME"/.i3/config ]; then
   mv "$HOME"/.i3/config "$HOME"/.i3/.config_backup
 fi
 
-CONFIGS=$(find "$HOME"/.i3/core/* -printf "%f\n")
-for conf in $CONFIGS; do
-  printf "\n" >> "$HOME"/.i3/config
+for conf in $(ls "$HOME"/.i3/core); do
+  echo -e "\n" >> "$HOME"/.i3/config
   cat "$HOME/.i3/core/$conf" >> "$HOME"/.i3/config
   if [ -e "$MODEL" ] && [ -e "$HOME/.i3/$MODEL/$conf" ]; then
-    printf "\n" >> "$HOME"/.i3/config
+    echo -e "\n" >> "$HOME"/.i3/config
     cat "$HOME/.i3/$MODEL/$conf" >> "$HOME"/.i3/config
   fi
 done
