a84582856e46129d909fb8f46a2fb8aa4d28fecf
Kyle Kneitinger <kyle@kneit.in>
2017-06-15 19:53:34 -0700

    Switch T450s display names to match Intel driver

--- .screenlayout/thinkpad_t450s/2ext_dvi-left-port_vga-right-port.sh
+++ .screenlayout/thinkpad_t450s/2ext_dvi-left-port_vga-right-port.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 1080x1296 --rotate normal --output DP-2 --mode 1920x1080 --pos 3000x0 --rotate left
+xrandr --output HDMI-2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-1 --off --output eDP1 --primary --mode 1920x1080 --pos 1080x1296 --rotate normal --output DP2 --mode 1920x1080 --pos 3000x0 --rotate left
