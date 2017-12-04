74031f98a6e0dd23026b1d297c2525cedf703666
Kyle Kneitinger <kyle@kneit.in>
2017-12-04 00:52:29 -0800

    Initial polybar configuration

---
+++ .i3/bar_scripts/polybar_launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+#while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bars
+for i in $(polybar -m | awk -F: '{print $1}'); do
+    MONITOR=$i polybar example -c ~/.i3//polybar.conf &
+done
+
+echo "Bars launched..."
