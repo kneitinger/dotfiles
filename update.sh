488e56b936d0a818b781d857363e5328107d6184
Kyle Kneitinger <kyle@kneit.in>
2018-01-30 23:49:28 -0800

    Remove old update.sh script

--- update.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-# Credit to Jessie Frazelle: https://github.com/jessfraz/.vim/blob/master/update.sh
-
-set -e
-
-git submodule update --init --recursive
-git submodule foreach git pull --recurse-submodules origin master
