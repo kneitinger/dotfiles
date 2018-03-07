737ad21a33c5cb4ca040331d20984787ecc3ec37
Kyle Kneitinger <kyle@kneit.in>
2018-03-06 16:16:51 -0800

    Add screenlayout files for troi

---
+++ .screenlayout/troi/vga_above.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1600x900 --pos 128x1080 --rotate normal --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal
