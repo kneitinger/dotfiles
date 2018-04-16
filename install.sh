1ead83ad9f2c4ddee9ee69861fcae328abdbe348
Kyle Kneitinger <kyle@kneit.in>
2018-04-15 18:58:33 -0700

    Change terminal/i3 font to Fantasque Sans Mono

--- install.sh
+++ install.sh
@@ -14,4 +14,10 @@ curl -L -o "$HOME"/.fonts/FontAwesome.otf \
     https://github.com/FortAwesome/Font-Awesome/raw/41b9ed01103e6820c3cb043ba7ddab30ecd3f4c0/fonts/FontAwesome.otf
 curl -L -o "$HOME"/.fonts/monaco.ttf \
     https://github.com/todylu/monaco.ttf/raw/master/monaco.ttf
+curl -L -o /tmp/fantasque_mono.tar.gz \
+    https://github.com/belluzj/fantasque-sans/releases/download/v1.7.2/FantasqueSansMono-Normal.tar.gz
+mkdir -p /tmp/fantasque_mono && tar xzfv /tmp/fantasque_mono.tar.gz -C /tmp/fantasque_mono 
+cp /tmp/fantasque_mono/TTF/* ~/.fonts
+rm -rf /tmp/fantasque_mono*
+
 fc-cache -f ~/.fonts
