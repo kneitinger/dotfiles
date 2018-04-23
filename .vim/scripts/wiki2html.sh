6ddeb3e18340cf62796d1a060737ec220312c726
Kyle Kneitinger <kyle@kneit.in>
2018-04-22 22:26:57 -0700

    Add vimwiki for notetaking and md->html conversion script

---
+++ .vim/scripts/wiki2html.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Adapted from
+# https://gist.github.com/enpassant/0496e3db19e32e110edca03647c36541
+
+SYNTAX="$2"
+OUTPUTDIR="$4"
+INPUT="$5"
+
+
+FILE=$(basename "$INPUT" | cut -d'.' -f1)
+OUTPUT="$OUTPUTDIR$FILE.html"
+CSSFILENAME=$(basename "$6")
+
+HAS_MATH=$(grep -o '\$\$.\+\$\$' "$INPUT")
+if [ ! -z "$HAS_MATH" ]; then
+    MATH="--mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+else
+    MATH=
+fi
+
+sed -r 's/(\[.+\])\(([^)]+)\)/\1(\2.html)/g' < "$INPUT" \
+    | pandoc $MATH -s -f "$SYNTAX" -t html -c "$CSSFILENAME" \
+    | sed -r 's/<li>(.*)\[ \]/<li class="todo done0">\1/g; s/<li>(.*)\[X\]/<li class="todo done4">\1/g' \
+    > "$OUTPUT"
+
