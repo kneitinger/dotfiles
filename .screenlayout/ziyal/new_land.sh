be63ba383615f76ea0f99272249affb52d3810a0
Kyle Kneitinger <kyle@kneit.in>
2018-05-01 11:29:49 -0700

    Rename ziyal landscape screen layout script

--- .screenlayout/ziyal/new_land.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP1-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP2 --off
