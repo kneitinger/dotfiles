f56515e52e33f096de2fa28d837e893a5a701845
Kyle Kneitinger <kyle@kneit.in>
2018-07-01 01:29:17 -0700

    Janeway reinstall, add new screenlayouts

--- .screenlayout/janeway/2ext_dvi-left-land_vga-right-land.sh
+++ .screenlayout/janeway/2ext_dvi-left-land_vga-right-land.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output eDP1 --primary --mode 1920x1080 --pos 968x1080 --rotate normal --output DP2 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 984x1080 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
