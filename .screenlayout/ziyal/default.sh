44f894551c0e64c1697ddf297997a2785fd6405c
Kyle Kneitinger <kyle@kneit.in>
2019-03-20 11:47:41 -0700

    [i3] update ziyal display names

--- .screenlayout/ziyal/default.sh
+++ .screenlayout/ziyal/default.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off \
+       --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+       --output DP-1 --off \
+       --output DP-2 --off \
+       --output DP-2-1 --off \
+       --output DP-2-2 --off \
+       --output DP-2-3 --off  \
+       --output HDMI-1 --off \
+       --output HDMI-2 --off
