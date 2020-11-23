118c33e95c3863b449eb102a4db42fe6993d56a1
Kyle Kneitinger <kyle@kneit.in>
2020-11-23 00:21:54 -0800

    [xorg] modify janeway dock_no-internal.sh layout

--- .screenlayout/janeway/dock_no-internal.sh
+++ .screenlayout/janeway/dock_no-internal.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
-xrandr --output VIRTUAL1 --off \
-       --output eDP1 --off \
-       --output DP1 --off \
-       --output DP2-1 --off \
-       --output DP2-2 --crtc 1 --mode 1920x1080 --pos 0x0 --rotate normal \
-       --output DP2-3 --primary --crtc 0 --mode 1920x1080 --pos 1920x0 --rotate normal \
-       --output DP2-3 --off \
-       --output HDMI2 --off \
-       --output HDMI1 --off \
-       --output DP2 --off
-
-
-
-
+#!/bin/sh
+xrandr \
+    --output eDP1 --off \
+    --output DP1 --off \
+    --output DP2 --off \
+    --output DP2-1 --off \
+    --output DP2-2 --mode 2560x1440 --pos 1920x0 --rotate normal \
+    --output DP2-3 --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output HDMI1 --off \
+    --output HDMI2 --off \
+    --output VIRTUAL1 --off
