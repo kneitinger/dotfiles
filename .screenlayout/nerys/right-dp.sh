b789e461183c32702d189eda50bbe9eaa49a527d
Kyle Kneitinger <kyle@kneit.in>
2020-11-29 18:07:37 -0800

    [xorg] Add initial nerys screenlayout files

---
+++ .screenlayout/nerys/right-dp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xrandr --output eDP --off \
+    --output HDMI-A-0 --off \
+    --output DisplayPort-0 --off \
+    --output DisplayPort-1 --off \
+    --output DisplayPort-2 --off \
+    --output DisplayPort-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+    --output DisplayPort-4 --off
