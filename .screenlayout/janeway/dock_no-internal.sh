9b8940fdca140ad9d05c3ce7091fe1a4c3cd8172
Kyle Kneitinger <kyle@kneit.in>
2020-12-02 19:09:22 -0800

    [xorg] Use common formatting in all screenlayout files
    The .sh file generated by `arandr` has the `xrandr` call on a single
    line. The new conventional format can be obtained via
    
      sed -i 's/\(\S \)\(--output\)/\1\\\n  \2/g' *
    
    which  is idempotent.

--- .screenlayout/janeway/dock_no-internal.sh
+++ .screenlayout/janeway/dock_no-internal.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 xrandr \
-    --output eDP1 --off \
-    --output DP1 --off \
-    --output DP2 --off \
-    --output DP2-1 --off \
-    --output DP2-2 --mode 2560x1440 --pos 1920x0 --rotate normal \
-    --output DP2-3 --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output HDMI1 --off \
-    --output HDMI2 --off \
-    --output VIRTUAL1 --off
+  --output eDP1 --off \
+  --output DP1 --off \
+  --output DP2 --off \
+  --output DP2-1 --off \
+  --output DP2-2 --mode 2560x1440 --pos 1920x0 --rotate normal \
+  --output DP2-3 --mode 1920x1080 --pos 0x0 --rotate normal \
+  --output HDMI1 --off \
+  --output HDMI2 --off \
+  --output VIRTUAL1 --off
--- .screenlayout/janeway/right.sh
+++ .screenlayout/janeway/right.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr \
+  --output VIRTUAL1 --off \
+  --output eDP1 --off \
+  --output DP1 --off \
+  --output DP2-1 --off \
+  --output DP2-2 --off \
+  --output DP2-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+  --output HDMI2 --off \
+  --output HDMI1 --off \
+  --output DP2 --off
