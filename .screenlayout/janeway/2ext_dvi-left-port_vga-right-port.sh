a7b7f5f8347923801d1c53fa80f9803220e3d797
Kyle Kneitinger <kyle@kneit.in>
2020-12-02 19:04:42 -0800

    [xorg] Remove outdated host janeway screenlayout files

--- .screenlayout/janeway/2ext_dvi-left-port_vga-right-port.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-1 --off --output eDP1 --primary --mode 1920x1080 --pos 1080x1296 --rotate normal --output DP2 --mode 1920x1080 --pos 3000x0 --rotate left
