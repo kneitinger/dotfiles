77b19c6a60e74ff2af4ce87917163abd689fe652
Kyle Kneitinger <kyle@kneit.in>
2023-10-01 12:09:41 -0700

    [xorg] Make tilly dock layouts more resilient

--- .screenlayout/tilly/dock.sh
+++ .screenlayout/tilly/dock.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
+
+# Sometimes the monitors are detected as DP-3/DP-4, sometimes DP-5/DP-6...try both
 xrandr \
   --output eDP --off \
   --output HDMI-A-0 --off \
   --output DisplayPort-0 --off \
   --output DisplayPort-1 --off \
   --output DisplayPort-2 --off \
-  --output DisplayPort-3 --mode 2560x1440 --pos 0x0 --rotate normal \
-  --output DisplayPort-4 --primary --mode 2560x1440 --pos 2560x0 --rotate normal
+  --output DisplayPort-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+  --output DisplayPort-4 --mode 2560x1440 --pos 2560x0 --rotate normal \
+  --output DisplayPort-5 --off \
+  --output DisplayPort-6 --off \
+|| \
+xrandr \
+  --output eDP --off \
+  --output HDMI-A-0 --off \
+  --output DisplayPort-0 --off \
+  --output DisplayPort-1 --off \
+  --output DisplayPort-2 --off \
+  --output DisplayPort-3 --off \
+  --output DisplayPort-4 --off \
+  --output DisplayPort-5 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+  --output DisplayPort-6 --mode 2560x1440 --pos 2560x0 --rotate normal
--- .screenlayout/tilly/left-dp.sh
+++ .screenlayout/tilly/left-dp.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# Sometimes the left monitor gets detected as DP-3, sometime DP-5...try both
 xrandr \
   --output eDP --off \
   --output HDMI-A-0 --off \
@@ -6,4 +8,17 @@ xrandr \
   --output DisplayPort-1 --off \
   --output DisplayPort-2 --off \
   --output DisplayPort-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
-  --output DisplayPort-4 --off
+  --output DisplayPort-4 --off \
+  --output DisplayPort-5 --off \
+  --output DisplayPort-6 --off \
+|| \
+xrandr \
+  --output eDP --off \
+  --output HDMI-A-0 --off \
+  --output DisplayPort-0 --off \
+  --output DisplayPort-1 --off \
+  --output DisplayPort-2 --off \
+  --output DisplayPort-3 --off \
+  --output DisplayPort-4 --off \
+  --output DisplayPort-5 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+  --output DisplayPort-6 --off \
